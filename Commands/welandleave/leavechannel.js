f2290b0b8f4084da6e211722df86b4d2d7a3901d
Bling-blang <84137662+Bling-blang@users.noreply.github.com>
2021-05-13 22:18:17 +0800

    Add files via upload

---
+++ Commands/welandleave/leavechannel.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js')
+const fs = require('fs');
+const db = require('quick.db')
+module.exports.run = async (client, message, args, prefix) => { // for my cmds handler
+
+    if(!message.content.startsWith(prefix)) return;
+   let Channel = message.mentions.channels.first() || message.guild.channels.cache.get(args[0]) || message.guild.channels.cache.find(c => c.name.toLowerCase().includes(args[0]))
+        if (!Channel) return message.channel.send({
+            embed: {
+                title: 'Missing Channel',
+                description: 'Missing required arguments!',
+                fields: [{
+                    name: 'Usage',
+                    value: '`leavechannel <channelID | channelMention | channelName>`'
+                }],
+                timestamp: new Date(),
+                color: 'RED'
+            }
+        })
+        await db.set(`leave_${message.guild.id}`, Channel.id)
+        await message.channel.send({
+            embed: {
+                title: 'Success!',
+                description: `Leave channel set as: <#${Channel.id}>! All leave messages will be redirected here. If this was a mistake, please configure it again`,
+                color: 'GREEN',
+                timestamp: new Date()
+            }
+        })
+    }
+    module.exports.help = {
+    name: 'leavechannel',
+    aliases: []
+}
